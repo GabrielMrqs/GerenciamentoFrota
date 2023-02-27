f968358fbac2380b1b37e6c53bb56638041f9bf1
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2023-02-27 10:11:50 -0300

    Implementado filtro por chassi na grid

--- client/src/app/veiculos/components/listar/listar.component.ts
+++ client/src/app/veiculos/components/listar/listar.component.ts
@@ -1,21 +1,31 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Veiculo } from '../../models/veiculo.model';
 import { TipoVeiculo } from '../../models/tipo-veiculo';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html',
   styleUrls: ['./listar.component.scss']
 })
-export class ListarComponent {
+export class ListarComponent implements OnInit {
+
 
   @Input() veiculos: Veiculo[];
   @Output() add = new EventEmitter(false);
   @Output() edit = new EventEmitter(false);
   @Output() delete = new EventEmitter(false);
+  dataSource: MatTableDataSource<Veiculo>;
 
-  displayedColumns = ['chassi', 'tipoVeiculo', 'cor', 'numeroPassageiros', 'actions'];
   
+  displayedColumns = ['chassi', 'tipoVeiculo', 'cor', 'numeroPassageiros', 'actions'];
+  ngOnInit(): void {
+    this.dataSource =  new MatTableDataSource(this.veiculos);
+    this.dataSource.filterPredicate = (data: Veiculo, filter: string) => {
+      return data.chassi == filter;
+     };
+  }
+
   onAdd() {
     this.add.emit(true);
   }
@@ -32,4 +42,8 @@ export class ListarComponent {
     return TipoVeiculo[tipoVeiculo];
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
